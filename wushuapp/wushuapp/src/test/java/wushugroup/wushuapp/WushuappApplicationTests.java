412abc3dec1e155e48f10bb65c130323039a65ca
KingDolphin123 <35504923+KingDolphin123@users.noreply.github.com>
2024-11-26 17:06:03 -0500

    backend
    backend

---
+++ wushuapp/wushuapp/src/test/java/wushugroup/wushuapp/WushuappApplicationTests.java
@@ -0,0 +1,13 @@
+package wushugroup.wushuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WushuappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
